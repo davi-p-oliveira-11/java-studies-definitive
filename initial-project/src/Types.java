3d6b29139ce5b1d0ca8518f278bdfa0327543367
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-07-01 17:01:58 -0300

    added types class

---
+++ initial-project/src/Types.java
@@ -0,0 +1,21 @@
+public class Types {
+
+    public static void main(String[] args) {
+        // Integer types
+        byte myByte = 100;
+        short myShort = 10000;
+        int myInt = 100000;
+        long myLong = 10000000000L;
+
+        // Floating point types
+        float myFloat = 10.5f;
+        double myDouble = 20.99;
+
+        // Character type
+        char myChar = 'A';
+
+        // Boolean type
+        boolean myBoolean = true;
+    }
+
+}
