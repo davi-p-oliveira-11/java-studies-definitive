44eb586d4077a878a7f211bed91780cda2038b78
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-07-08 17:55:45 -0300

    updated oop folder

---
+++ oop/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+        var male = new Person("Joao");
+        male.incAge();
+        var female = new Person("Maria");
+        female.incAge();
+        System.out.println("Male name: " + male.getName() + " age: " + male.getAge());
+        System.out.println("Female name: " + female.getName() + " age: " + female.getAge());
+    }
+
+}
